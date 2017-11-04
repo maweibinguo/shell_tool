f632f0afdf25023457d6d0d990572caf5f5c4e90
maweibinguo@163.com <maweibinguo@163.com>
2017-11-04 11:00:31 +0800

    php安装完成

--- software_install/php-install/install.sh
+++ software_install/php-install/install.sh
@@ -7,7 +7,8 @@
 # 初始化基础的依赖
 function init_php_base_dependences()
 {
-   yum -y install gcc gcc-c++ autoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers gd gd2 gd-devel gd2-devel perl-CPAN pcre-devel gmp-devel epel-release libmcrypt-devel readline-devel libxslt-devel
+   yum install -y epel-release
+   yum -y install gcc gcc-c++ autoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers gd gd2 gd-devel gd2-devel perl-CPAN pcre-devel gmp-devel  libmcrypt-devel readline-devel libxslt-devel
     return 1
 }
 
